dcda868adbe38005e225d8d58aa832fd6ac196fd
Roman Khabayev <supernaturalmlady@gmail.com>
2023-05-25 19:49:32 +0600

    project created, getting data with api released

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
+import { ProductService } from './services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-course';
+export class AppComponent implements OnInit {
+  title = 'angular app';
+  products: IProduct[] = [];
+
+  constructor(private productsService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe(products => {
+      this.products = products
+    });
+  }
 }
